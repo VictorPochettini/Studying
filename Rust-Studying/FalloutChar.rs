6d5e12cb4182c6e5b413893d4e20486edf217ffe
Victor <vpochettini@gmail.com>
2025-11-04 00:04:01 -0300

    feat(skills): added skill displaying and created the array

--- Rust-Studying/FalloutChar.rs
+++ Rust-Studying/FalloutChar.rs
@@ -8,12 +8,22 @@ fn main()
     let mut CHA = 5;
     let mut INT = 5;
     let mut AGY = 5;
-    let mut LUC = 5;    
+    let mut LUC = 5;
+
+    let mut skills = [&STR, &PER, &END, &CHA, &INT, &AGY, &LUC];
 
     println!("Welcome to the Fallout character creator");
     loop 
     {
-        println!("Aqui estão seus atributos atuais:");    
+        println!("Aqui estão seus atributos atuais:");
+
+        println!("STR: {}", STR);
+        println!("PER: {}", PER);
+        println!("END: {}", END);
+        println!("CHA: {}", CHA);
+        println!("INT: {}", INT);
+        println!("AGY: {}", AGY);
+        println!("LUC: {}", LUC);
     }
     
 }
