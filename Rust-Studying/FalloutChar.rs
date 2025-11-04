f23edae559b0344cc19e8903199578baf705ce98
Victor <vpochettini@gmail.com>
2025-11-03 23:55:32 -0300

    feat(skills): wrote the character skills

--- Rust-Studying/FalloutChar.rs
+++ Rust-Studying/FalloutChar.rs
@@ -1,4 +1,19 @@
 fn main()
 {
+    let mut estado= 0;
+
+    let mut STR = 5;
+    let mut PER = 5;  
+    let mut END = 5;
+    let mut CHA = 5;
+    let mut INT = 5;
+    let mut AGY = 5;
+    let mut LUC = 5;    
+
+    println!("Welcome to the Fallout character creator");
+    loop 
+    {
+        println!("Aqui estão seus atributos atuais:");    
+    }
     
 }
